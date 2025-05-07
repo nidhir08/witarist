35bcf36c72149d827531264028218c80e1b00f99
Nidhi Rawat <168139652+nidhir08@users.noreply.github.com>
2025-05-07 19:33:23 +0530

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@radix-ui/react-separator";
 import Image from "next/image";
 import Header from "./header";
 import Navbar from "./navbar";
-import { ArrowDown, ArrowDownRight, ArrowUp, ArrowUpRight, CalendarDays, ChevronDown, ChevronUp, ClipboardCopy, CloudDownload, Edit, FlagTriangleRight, ImagePlay, ListFilter, NotepadText, OctagonAlert, Plus, Scroll, Search, Store, ThumbsDown, ThumbsUp, Trash } from "lucide-react";
+import { ArrowDown, ArrowDownRight, ArrowUp, ArrowUpRight, CalendarDays, ChevronDown, ChevronUp, ClipboardCopy, CloudDownload, Edit, FlagTriangleRight, ImagePlay, ListFilter, NotepadText, OctagonAlert, Plus, Scroll, Search, Store, ThumbsDown, ThumbsUp, Trash, Clipboard } from "lucide-react";
 import { Doughnut } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Button } from "@/components/ui/button";
@@ -32,7 +32,7 @@ export default function Home() {
     datasets: [
       {
         data: ['50.5','30.5','19'],
-        backgroundColor: ['#1E293B', '#1E293BCC', '#1E293B33'],
+        backgroundColor: ['#2066A8', '#8CC5E399', '#3594CC33'],
         borderWidth: 1,
       },
     ],
@@ -48,15 +48,15 @@ export default function Home() {
     </div>
     <div className="flex flex-col w-full flex-grow">
     <Header />
-    <Separator orientation="horizontal" className="w-full h-[2px] bg-[#E5E5E5] m-0"/>
+    <Separator orientation="horizontal" className="w-full h-[1px] bg-[#E5E5E5] m-0"/>
     <div className="px-10 text-2xl font-bold text-[#171718] mt-4 flex flex-row  gap-160 ">
     Dashboard
-    <div className="flex flex-row gap-2 ml-4 text-[16px] font-medium text-[#475569] border-2 border-[#E2E8F0]  rounded-[15px] w-[310px] bg-white">
+    <div className="flex flex-row gap-2 ml-4 text-[16px] font-medium text-[#475569] border-1 border-[#E2E8F0]  rounded-[15px] w-[310px] bg-white">
       <div className="flex flex-row gap-2 px-3 pt-2 pb-2 items-center">
     <CalendarDays />
     Oct 18 - Nov 18
     </div>
-    <Separator orientation="vertical" className="h-12 w-[2px] bg-[#E5E5E5] m-0"/>
+    <Separator orientation="vertical" className="h-12 w-[1px] bg-[#E5E5E5] m-0"/>
     <div className="flex flex-row gap-2 px-2 pt-2 items-center pb-2">
     Monthly
     <ChevronDown />
@@ -66,18 +66,18 @@ export default function Home() {
 
 
      {/* componets */}
-  <div className="flex flex-row bg-[#FCFBFA] mt-6 px-5 gap-4">
+<div className="flex flex-row bg-[#FCFBFA] mt-6 px-5 gap-3">
 
     {/* component1 */}
-    <div className="flex flex-col w-[270px] h-[150px] bg-white border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
+<div className="flex flex-col w-[216px] h-[141px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-4">
   {/* Vendors Added Section (Text + Icon in same row) */}
   <div className="flex flex-row justify-between items-center gap-4">
-    <div className="flex flex-col">
+    <div className="flex flex-col pt-3 text-[13px]">
       Vendors Added
       <h1 className="text-black font-bold text-[20px]">578</h1>
     </div>
     
-    <div className="flex items-center justify-center w-[45px] h-[45px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-3">
+    <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-2">
       <Store className="w-12 h-12" />
     </div>
   </div>
@@ -88,9 +88,9 @@ export default function Home() {
   </div>
   
   <div className="flex flex-row justify-between items-center gap-4 mt-0 ">
-  <p className="text-[14px] font-medium text-[#16A34A]">+6.26% from last month</p>
-  <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#c9f6d9] border-[#E5E5E5] rounded-md ">
-  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#16A34A] border-[#E5E5E5] rounded-md ">
+  <p className="text-[12px] font-light text-[#16A34A]">+6.26% from last month</p>
+  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#c9f6d9] border-[#E5E5E5] rounded-md ">
+  <div className="flex items-center justify-center w-[20px] h-[20px] bg-[#16A34A] border-[#E5E5E5] rounded-md ">
   <ArrowUpRight className="text-white w-[15px] h-[15px]" />
   </div>
  </div>
@@ -98,16 +98,16 @@ export default function Home() {
 </div>
 
 {/* component2 */}
-<div className="flex flex-col w-[270px] h-[150px] bg-white border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
+<div className="flex flex-col w-[216px] h-[141px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-4">
   {/* Vendors Added Section (Text + Icon in same row) */}
   <div className="flex flex-row justify-between items-center gap-4">
-    <div className="flex flex-col">
+    <div className="flex flex-col font-medium pt-3 text-[13px]">
     Bench Utilization Rate 
       <h1 className="text-black font-bold text-[20px]">34%</h1>
     </div>
     
-    <div className="flex items-center justify-center w-[45px] h-[45px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-3">
-      <ImagePlay className="w-12 h-12" /> 
+    <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-2">
+      <ImagePlay className="w-12 h-12 " /> 
     </div>
   </div>
 
@@ -117,9 +117,9 @@ export default function Home() {
   </div>
   
   <div className="flex flex-row justify-between items-center gap-4 mt-0 ">
-  <p className="text-[14px] font-medium text-[#EF4444]">-6.26% from last month</p>
-  <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#f99c9c] border-[#E5E5E5] rounded-md ">
-  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#EF4444] border-[#E5E5E5] rounded-md ">
+  <p className="text-[12px] font-light text-[#EF4444]">-6.26% from last month</p>
+  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#f99c9c] border-[#E5E5E5] rounded-md ">
+  <div className="flex items-center justify-center w-[20px] h-[20px] bg-[#EF4444] border-[#E5E5E5] rounded-md ">
   <ArrowDownRight className="text-white w-[15px] h-[15px]"  /> 
   </div>
  </div>
@@ -127,15 +127,45 @@ export default function Home() {
 </div>
 
 {/* component3 */}
-<div className="flex flex-col w-[270px] h-[150px] bg-white border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
+<div className="flex flex-col w-[216px] h-[141px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-4">
   {/* Vendors Added Section (Text + Icon in same row) */}
   <div className="flex flex-row justify-between items-center gap-4">
-    <div className="flex flex-col">
-    Deployments
+    <div className="flex flex-col pt-3 text-[13px]">
+    Engagement Rate 
       <h1 className="text-black font-bold text-[20px]">115</h1>
     </div>
     
-    <div className="flex items-center justify-center w-[45px] h-[45px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-3">
+    <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-2">
+    <Clipboard  className="w-12 h-12" />
+    </div>
+  </div>
+
+  {/* Separator */}
+  <div>
+    <Separator orientation="horizontal" className="h-[2px] w-full bg-[#E5E5E5]" />
+  </div>
+  
+  <div className="flex flex-row justify-between items-center gap-4 mt-0 ">
+  <p className="text-[12px] font-light text-[#16A34A]">+6.26% from last month</p>
+  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#c9f6d9] border-[#E5E5E5] rounded-md ">
+  <div className="flex items-center justify-center w-[20px] h-[20px] bg-[#16A34A] border-[#E5E5E5] rounded-md ">
+  <ArrowUpRight className="text-white w-[15px] h-[15px]" />
+  </div>
+ </div>
+    </div>
+</div>
+
+
+{/* component4 */}
+<div className="flex flex-col w-[216px] h-[141px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium  pl-3 pr-3 gap-4">
+  {/* Vendors Added Section (Text + Icon in same row) */}
+  <div className="flex flex-row justify-between items-center gap-4 ">
+    <div className="flex flex-col pt-3 text-[13px] ">
+    Deployments
+      <h1 className="text-black font-bold text-[20px]">57</h1>
+    </div>
+    
+    <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#F5F5F5] border-[#E5E5E5] rounded-md p-2">
     <NotepadText className="w-12 h-12" /> 
     </div>
   </div>
@@ -146,20 +176,20 @@ export default function Home() {
   </div>
   
   <div className="flex flex-row justify-between items-center gap-4 mt-0 ">
-  <p className="text-[14px] font-medium text-[#16A34A]">+6.26% from last month</p>
-  <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#c9f6d9] border-[#E5E5E5] rounded-md ">
-  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#16A34A] border-[#E5E5E5] rounded-md ">
-  <ArrowUpRight className="text-white w-[15px] h-[15px]" />
+  <p className="text-[12px] font-light text-[#EF4444]">-6.26% from last month</p>
+  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#f99c9c] border-[#E5E5E5] rounded-md ">
+  <div className="flex items-center justify-center w-[20px] h-[20px] bg-[#EF4444] border-[#E5E5E5] rounded-md ">
+  <ArrowDownRight className="text-white w-[15px] h-[15px]"  /> 
   </div>
  </div>
     </div>
 </div>
 
-{/* component4 */}
-<div className="flex flex-col w-[270px] h-[150px] bg-white border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
+{/* component5 */}
+<div className="flex flex-col w-[216px] h-[141px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium  pl-3 pr-3 gap-4">
   {/* Vendors Added Section (Text + Icon in same row) */}
   <div className="flex flex-row justify-between items-center gap-4">
-    <div className="flex flex-col">
+    <div className="flex flex-col pt-3 text-[13px] ">
     Total Purchase
       <h1 className="text-black font-bold text-[20px]">57</h1>
     </div>
@@ -175,21 +205,22 @@ export default function Home() {
   </div>
   
   <div className="flex flex-row justify-between items-center gap-4 mt-0 ">
-  <p className="text-[14px] font-medium text-[#16A34A]">+6.26% from last month</p>
-  <div className="flex items-center justify-center w-[35px] h-[35px] bg-[#c9f6d9] border-[#E5E5E5] rounded-md ">
-  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#16A34A] border-[#E5E5E5] rounded-md ">
-  <ArrowUpRight className="text-white w-[15px] h-[15px]" />
+  <p className="text-[12px] font-light text-[#EF4444]">-6.26% from last month</p>
+  <div className="flex items-center justify-center w-[25px] h-[25px] bg-[#f99c9c] border-[#E5E5E5] rounded-md ">
+  <div className="flex items-center justify-center w-[20px] h-[20px] bg-[#EF4444] border-[#E5E5E5] rounded-md ">
+  <ArrowDownRight className="text-white w-[15px] h-[15px]"  /> 
   </div>
  </div>
     </div>
 </div>
 
 
-  </div>
+
+</div>
 
 {/* payment terms */}
 <div className="flex flex-row bg-[#FCFBFA] mt-6 px-5 gap-4 relative overflow-hidden">
-  <div className="flex flex-col w-[335px] h-[336px] bg-white border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
+  <div className="flex flex-col w-[335px] h-[336px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
 
     <div className="text-xl text-black">Payment Terms</div>
 
@@ -201,26 +232,26 @@ export default function Home() {
       </div>
 
       {/* Manual Labels */}
-      <div className="flex flex-col gap-2 text-sm text-[#475569]">
-        <div className="flex items-center gap-2">
+      <div className="flex flex-col gap-2 text-sm text-[#475569]  font-normal text-[12px]">
+        <div className="flex items-center gap-2 ">
           <div className="w-3 h-3 bg-[#1E293B] rounded-full" />
-          Net 15
+          Under Review
         </div>
         <div className="flex items-center gap-2">
           <div className="w-3 h-3 bg-[#47505F] rounded-full" />
-          Net 30
+          Interview
         </div>
         <div className="flex items-center gap-2">
           <div className="w-3 h-3 bg-[#6A727E] rounded-full" />
-          Net 30
+          Onhold
         </div>
         <div className="flex items-center gap-2">
           <div className="w-3 h-3 bg-[#B7BBC1] rounded-full" />
-          Net 30
+          Rejected
         </div>
         <div className="flex items-center gap-2">
           <div className="w-3 h-3 bg-[#DDDFE2] rounded-full" />
-          Net 30
+          Shortlisted
         </div>
       </div>
     </div>
@@ -233,7 +264,7 @@ export default function Home() {
 </div>
 
 {/* location */}
-  <div className="flex flex-col w-[774px] h-[336px] bg-white border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
+  <div className="flex flex-col w-[774px] h-[336px] bg-white border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4">
 
     <div className="flex flex-row text-xl text-black items-center justify-between ">Location Distribution
     <div className="flex flex-row ml-60 text-[16px] font-medium text-[#475569] border-2 border-[#E2E8F0]  rounded-[15px] w-[275px] bg-white overflow-hidden">
@@ -324,7 +355,7 @@ export default function Home() {
 
 {/* vendors activity */}
 <div className="flex flex-row bg-[#FCFBFA] mt-6 px-5 gap-4 relative overflow-hidden ">
-  <div className="flex flex-col w-[555px] h-[257px] bg-[neutral/200] border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-2">
+  <div className="flex flex-col w-[555px] h-[257px] bg-[neutral/200] border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-2">
     <h1 className="text-black font-bold pt-4 ">Vendors Activity (Till Now)</h1>
     <p className="font-light">Numbers of currently engaged vendors versus inactive ones.</p>
     <div className="flex flex-row items-center mt-2 gap-6">
@@ -362,7 +393,7 @@ export default function Home() {
 </div>
 </div>
 
-<div className="flex flex-col w-[555px] h-[257px] bg-[neutral/200] border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-2">
+<div className="flex flex-col w-[555px] h-[257px] bg-[neutral/200] border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pl-3 pr-3 gap-2">
     <h1 className="text-black font-bold pt-4 ">NDA’s Activity (Till Now)</h1>
     <p className="font-light">Numbers of currently signed and unsigned NDA’s</p>
     <div className="flex flex-row items-center mt-2 gap-6">
@@ -408,7 +439,7 @@ export default function Home() {
 </div>
 
 {/* company */}
-<div className="flex flex-col w-[1127px] h-[875px] mb-4 bg-[#FCFBFA] border-2 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4 ml-5 mt-6 mr-4">
+<div className="flex flex-col w-[1127px] h-[872px] mb-4 bg-[#FCFBFA] border-1 border-[#E5E5E5] rounded-[15px] text-[#475569] font-medium pt-4 pl-3 pr-3 gap-4 ml-5 mt-6 mr-4">
   <div className="flex flex-row gap-100">
     <div className="flex flex-col">
       <div className="flex flex-row gap-2">
@@ -878,7 +909,7 @@ export default function Home() {
 
  </div>
 {/* Row8 */}
-<div className=" flex flex-row gap-x-160 px-1 py-4 border-b border-[#E2E8F0] items-center hover:bg-[#F8FAFC] text-sm text-[#4A5568]">
+<div className=" flex flex-row gap-x-160 px-1 py-4  border-[#E2E8F0] items-center hover:bg-[#F8FAFC] text-sm text-[#4A5568]">
   <div className="flex items-center gap-4 px-10 font-medium text-lg">
     <h1>Page 1 of 10</h1>
   </div>
